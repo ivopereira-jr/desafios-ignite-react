80cf74209cd229bfbff36036e68963c2c9e777a0
ivopereira-jr <ivopereira.jr12@gmail.com>
2023-01-21 18:30:28 -0300

    :ok_hand: Ajustes background header

--- github-blog/src/components/Header/styles.ts
+++ github-blog/src/components/Header/styles.ts
@@ -5,7 +5,7 @@ import headerBg from '../../assets/headerBackground.png';
 export const HeaderContainer = styled.header`
 	width: 100%;
 	height: 18.5rem;
-	background: url(${headerBg}) no-repeat center center;
+	background: url(${headerBg}) no-repeat center / cover;
 	display: flex;
 	justify-content: center;
 
