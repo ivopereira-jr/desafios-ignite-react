6cc3ce1fdd55deeb1d94eacb628afb339870e041
ivopereira-jr <ivopereira.jr12@gmail.com>
2023-01-30 20:27:51 -0300

    :sparkles: Ajuste para poder enviar multiplos produtos

--- ignite-shop/src/pages/api/checkout.ts
+++ ignite-shop/src/pages/api/checkout.ts
@@ -1,19 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Product } from '../../contexts/CartContext';
 import { stripe } from '../../lib/stripe';
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const { priceId } = req.body;
+	const { products } = req.body as { products: Product[] };
 
 	if (req.method !== 'POST') {
 		return res.status(405).json({ error: 'Method not allowed.' });
 	}
 
-	if (!priceId) {
-		return res.status(400).json({ error: 'Price not found.' });
+	if (!products) {
+		return res.status(400).json({ error: 'Products not found.' });
 	}
+
+	const productsToLineItems = products.map(product => {
+		return {
+			price: product.defaultPriceId,
+			quantity: 1
+		};
+	});
 
 	const sucessUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
 	const cancelUrl = `${process.env.NEXT_URL}/`;
@@ -22,12 +30,7 @@ export default async function handler(
 		success_url: sucessUrl,
 		cancel_url: cancelUrl,
 		mode: 'payment',
-		line_items: [
-			{
-				price: priceId,
-				quantity: 1
-			}
-		]
+		line_items: productsToLineItems
 	});
 
 	return res.status(201).json({
