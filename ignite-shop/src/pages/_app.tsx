313158d801a92b0349c2aceb8ded069a80a17167
ivopereira-jr <ivopereira.jr12@gmail.com>
2023-01-30 17:43:51 -0300

    :sparkles: Integrando o contexto do carrinho

--- ignite-shop/src/pages/_app.tsx
+++ ignite-shop/src/pages/_app.tsx
@@ -1,20 +1,20 @@
 import { AppProps } from 'next/app';
-import Image from 'next/image';
 
-import logoImg from '../assets/logo.svg';
+import { CartContextProvider } from '../context/CartContext';
+import { Header } from '../components/Header';
+
 import { globalStyles } from '../styles/global';
-import { Container, Header } from '../styles/pages/app';
+import { Container } from '../styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Container>
-			<Header>
-				<Image src={logoImg} alt="" />
-			</Header>
-
-			<Component {...pageProps} />
+			<CartContextProvider>
+				<Header />
+				<Component {...pageProps} />
+			</CartContextProvider>
 		</Container>
 	);
 }
